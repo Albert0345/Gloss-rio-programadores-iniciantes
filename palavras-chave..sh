b1a98fd841c22290687cdbcaafcf5a8111518301
Albert Rocha <albertmatheus73@gmail.com>
2025-05-06 10:19:52 -0300

    Create palavras-chave..sh

---
+++ palavras-chave..sh
@@ -0,0 +1,13 @@
+# Palavras-chave para SEO# Palavras-chave para SEO
+
+Este arquivo contém as palavras-chave que usamos para otimizar nosso glossário de programação para iniciantes e ajudar na sua descoberta nos motores de busca.
+
+## Palavras-chave principais:
+- **programação para iniciantes**
+- **glossário de programação**
+- **conceitos de programação**
+- **termos básicos de codificação**
+- **aprendizado autodidata em programação**
+- **desenvolvedor iniciante**
+
+Essas palavras-chave estão presentes no conteúdo do repositório e foram escolhidas para atingir um público que está começando no mundo da programação.
